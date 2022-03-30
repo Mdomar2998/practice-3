ea65e2dfa6986b2d4c0c7ab99d1d59f7a7ed47fc
mdomar2998 <95675143+Mdomar2998@users.noreply.github.com>
2022-03-30 16:42:28 +0000

    p12 debug

---
+++ p12original.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+int fact(int x){
+    
+    if(x == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return x * fact(x-1);
+    }
+}
+
+
+float get_term(int x,int i){
+    float term;
+    term = (pow(x,i)/fact((2*i) + 1));
+    return term;
+}
+
+float sum(int x){
+    float sum= 1.0, term, prev_term = 1; 
+    int i = 1;
+    do{
+        /*
+        sum = sum + term;
+        term = (pow(x,i)/fact((2*i) + 1));
+        term = get_term(x,i++);
+        */
+        term = prev_term * x/((i+1)*(i+2));
+        sum += term;
+        prev_term = term;
+        i+=2;
+    
+    } while (term>0.000001);
+    return sum;
+}
+
+
+int input(){
+    int x;
+    printf("Enter the number to find the borga of : \t");
+    scanf("%d",&x);
+    return x;
+}
+
+int main(){
+    time_t seconds,first,second;
+    first = time(NULL);
+    int x;
+    float result;
+    x = input();
+    result = sum(x);
+    printf("Borga is : %lf \n",result);
+    second = time(NULL);
+    seconds = second - first;
+    printf("%ld \n",seconds);
+    return 0;
+
+}
